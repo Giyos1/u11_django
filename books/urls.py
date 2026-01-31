a08c0a7d7f02e01b6a0fd817cd715013b1e13f66
Giyos1 <giyosoripov4@gmail.com>
2026-01-31 20:10:42 +0500

    django form

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from books.views import home, book_list, book_detail, book_delete
+from books.views import home, book_list, book_detail, book_delete, book_create, book_create_post, book_update
 
 urlpatterns = [
     path('', home),
     path('list/', book_list, name='book_list'),
     path('detail/<int:pk>/', book_detail, name='book_detail'),
-    path('delete/<int:pk>/', book_delete, name='book_delete')
+    path('delete/<int:pk>/', book_delete, name='book_delete'),
+    path('create/', book_create, name='book_create'),  # formani ochib beropti
+    path('create_post/', book_create_post, name='book_create_post'),  # formani saqlashi kerak
+    path('update/<int:pk>/', book_update, name='book_update'),  # formani saqlashi kerak
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render, redirect
 
 from books.models import Book
@@ -20,3 +22,30 @@ def book_detail(request, pk=None):
 def book_delete(request, pk=None):
     Book.objects.filter(id=pk).delete()
     return redirect('book_list')
+
+
+def book_create(request):
+    return render(request, 'books/create.html')
+
+
+def book_create_post(request):
+    data = request.POST
+    Book.objects.create(
+        title=data.get('title'),
+        description=data.get('description'),
+        price=data.get('price')
+    )
+    return redirect('book_list')
+
+
+def book_update(request, pk=None):
+    book = Book.objects.filter(id=pk).first()
+    if request.method == 'POST':
+        data = request.POST
+        Book.objects.filter(id=pk).update(
+            title=data.get('title'),
+            description=data.get('description'),
+            price=data.get('price')
+        )
+        return redirect('book_detail', book.id)
+    return render(request, 'books/update.html', context={'book': book})
